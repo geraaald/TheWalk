ee00de5fdfbbcfcfe443e09a0a066e59b01e7378
matthew-brean <matthew.brean@ocsbstudent.ca>
2015-10-08 13:14:57 -0400

    moving

--- TheWalk/Form1.Designer.cs
+++ TheWalk/Form1.Designer.cs
@@ -29,13 +29,14 @@
         private void InitializeComponent()
         {
             this.picMan = new System.Windows.Forms.PictureBox();
+            this.btnWalk = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picMan)).BeginInit();
             this.SuspendLayout();
             // 
             // picMan
             // 
             this.picMan.Image = global::TheWalk.Properties.Resources.walk1;
-            this.picMan.Location = new System.Drawing.Point(52, 29);
+            this.picMan.Location = new System.Drawing.Point(130, 12);
             this.picMan.Name = "picMan";
             this.picMan.Size = new System.Drawing.Size(180, 180);
             this.picMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -43,11 +44,22 @@
             this.picMan.TabStop = false;
             this.picMan.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // btnWalk
+            // 
+            this.btnWalk.Location = new System.Drawing.Point(114, 211);
+            this.btnWalk.Name = "btnWalk";
+            this.btnWalk.Size = new System.Drawing.Size(75, 23);
+            this.btnWalk.TabIndex = 1;
+            this.btnWalk.Text = "WALK";
+            this.btnWalk.UseVisualStyleBackColor = true;
+            this.btnWalk.Click += new System.EventHandler(this.btnWalk_Click);
+            // 
             // frmTheWalk
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(322, 261);
+            this.Controls.Add(this.btnWalk);
             this.Controls.Add(this.picMan);
             this.Name = "frmTheWalk";
             this.Text = "The Walk";
@@ -60,6 +72,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox picMan;
+        private System.Windows.Forms.Button btnWalk;
     }
 }
 
--- TheWalk/Form1.cs
+++ TheWalk/Form1.cs
@@ -28,58 +28,74 @@ namespace TheWalk
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            int loopCounter = 0;
 
-            do
+        }
+        private void btnWalk_Click(object sender, EventArgs e)
+        {
             {
-                if (loopCounter == 0)
-                {
-                    this.picMan.Image = TheWalk.Properties.Resources.walk1;
-                }
-                else if (loopCounter == 1)
-                {
-                    this.picMan.Image = TheWalk.Properties.Resources.walk2;
-                }
-                else if (loopCounter == 2)
-                {
-                    this.picMan.Image = TheWalk.Properties.Resources.walk3;
-                }
-                else if (loopCounter == 3)
-                {
-                    this.picMan.Image = TheWalk.Properties.Resources.walk4;
-                }
-                else if (loopCounter == 4)
-                {
-                    this.picMan.Image = TheWalk.Properties.Resources.walk5;
-                }
-                else if (loopCounter == 5)
-                {
-                    this.picMan.Image = TheWalk.Properties.Resources.walk6;
-                }
-                else if (loopCounter == 6)
-                {
-                    this.picMan.Image = TheWalk.Properties.Resources.walk7;
-                }
-                else if (loopCounter == 7)
-                {
-                    this.picMan.Image = TheWalk.Properties.Resources.walk8;
-                }
-                else if (loopCounter == 8)
-                {
-                    this.picMan.Image = TheWalk.Properties.Resources.walk9;
-                }
-                else if (loopCounter == 9)
-                {
-                    this.picMan.Image = TheWalk.Properties.Resources.walk10;
-                }
+                int loopCounter = 0;
 
-                loopCounter = loopCounter + 1;
+                do
+                {
+                    if (loopCounter == 0)
+                    {
+                        this.picMan.Image = TheWalk.Properties.Resources.walk1;
+                    }
+                    else if (loopCounter == 1)
+                    {
+                        this.picMan.Image = TheWalk.Properties.Resources.walk2;
+                        this.picMan.Left = this.picMan.Left - 10;
+                    }
+                    else if (loopCounter == 2)
+                    {
+                        this.picMan.Image = TheWalk.Properties.Resources.walk3;
+                        this.picMan.Left = this.picMan.Left - 10;
+                    }
+                    else if (loopCounter == 3)
+                    {
+                        this.picMan.Image = TheWalk.Properties.Resources.walk4;
+                        this.picMan.Left = this.picMan.Left - 10;
+                    }
+                    else if (loopCounter == 4)
+                    {
+                        this.picMan.Image = TheWalk.Properties.Resources.walk5;
+                        this.picMan.Left = this.picMan.Left - 10;
+                    }
+                    else if (loopCounter == 5)
+                    {
+                        this.picMan.Image = TheWalk.Properties.Resources.walk6;
+                        this.picMan.Left = this.picMan.Left - 10;
+                    }
+                    else if (loopCounter == 6)
+                    {
+                        this.picMan.Image = TheWalk.Properties.Resources.walk7;
+                        this.picMan.Left = this.picMan.Left - 10;
+                    }
+                    else if (loopCounter == 7)
+                    {
+                        this.picMan.Image = TheWalk.Properties.Resources.walk8;
+                        this.picMan.Left = this.picMan.Left - 10;
+                    }
+                    else if (loopCounter == 8)
+                    {
+                        this.picMan.Image = TheWalk.Properties.Resources.walk9;
+                        this.picMan.Left = this.picMan.Left - 10;
+                    }
+                    else if (loopCounter == 9)
+                    {
+                        this.picMan.Image = TheWalk.Properties.Resources.walk10;
+                        this.picMan.Left = this.picMan.Left - 10;
+                    }
 
-                // pause
-                this.Refresh();
-                Thread.Sleep(100);
+                    loopCounter = loopCounter + 1;
 
-            } while (loopCounter < 10);
+                    // pause
+                    this.Refresh();
+                    Thread.Sleep(100);
+
+                } while (loopCounter < 10);
+            }
+
         }
     }
 }   
