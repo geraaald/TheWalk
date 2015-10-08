9a3faedebe111d54e343ec3ee12bba890307b5f2
matthew-brean <matthew.brean@ocsbstudent.ca>
2015-10-08 12:53:34 -0400

    Steady

---
+++ TheWalk/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace TheWalk
+{
+    partial class frmTheWalk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picMan = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picMan
+            // 
+            this.picMan.Image = global::TheWalk.Properties.Resources.walk1;
+            this.picMan.Location = new System.Drawing.Point(52, 29);
+            this.picMan.Name = "picMan";
+            this.picMan.Size = new System.Drawing.Size(180, 180);
+            this.picMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picMan.TabIndex = 0;
+            this.picMan.TabStop = false;
+            this.picMan.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // frmTheWalk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.picMan);
+            this.Name = "frmTheWalk";
+            this.Text = "The Walk";
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picMan;
+    }
+}
+
---
+++ TheWalk/Form1.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * Created by: Gerald Palma
+ * Created on: 08-Oct-2015
+ * Created for: ICS3U
+ * This program displays a walking man.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// for pause
+using System.Threading;
+
+namespace TheWalk
+{
+    public partial class frmTheWalk : Form
+    {
+        public frmTheWalk()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            int loopCounter = 0;
+
+            do
+            {
+                if (loopCounter == 0)
+                {
+                    this.picMan.Image = TheWalk.Properties.Resources.walk1;
+                }
+                else if (loopCounter == 1)
+                {
+                    this.picMan.Image = TheWalk.Properties.Resources.walk2;
+                }
+                else if (loopCounter == 2)
+                {
+                    this.picMan.Image = TheWalk.Properties.Resources.walk3;
+                }
+                else if (loopCounter == 3)
+                {
+                    this.picMan.Image = TheWalk.Properties.Resources.walk4;
+                }
+                else if (loopCounter == 4)
+                {
+                    this.picMan.Image = TheWalk.Properties.Resources.walk5;
+                }
+                else if (loopCounter == 5)
+                {
+                    this.picMan.Image = TheWalk.Properties.Resources.walk6;
+                }
+                else if (loopCounter == 6)
+                {
+                    this.picMan.Image = TheWalk.Properties.Resources.walk7;
+                }
+                else if (loopCounter == 7)
+                {
+                    this.picMan.Image = TheWalk.Properties.Resources.walk8;
+                }
+                else if (loopCounter == 8)
+                {
+                    this.picMan.Image = TheWalk.Properties.Resources.walk9;
+                }
+                else if (loopCounter == 9)
+                {
+                    this.picMan.Image = TheWalk.Properties.Resources.walk10;
+                }
+
+                loopCounter = loopCounter + 1;
+
+                // pause
+                this.Refresh();
+                Thread.Sleep(100);
+
+            } while (loopCounter < 10);
+        }
+    }
+}   
